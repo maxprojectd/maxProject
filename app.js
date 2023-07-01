dd9744a95817a5146f8687504c92d818e9d8bb4e
M4xIq <maxiq998@gmail.com>
2023-07-01 11:39:11 +0300

    stream

--- app.js
+++ app.js
@@ -205,3 +205,31 @@ function resetText() {
 const start = setInterval(scrambleText, 100)
 
 timeoutId = setTimeout(resetText, 2000)
+
+
+
+// timer start
+
+let selectedDate = new Date("2023/7/16").getTime()
+
+
+
+let counter = setInterval(() => {
+    let dateNow = new Date().getTime()
+    let dateDiff = selectedDate - dateNow
+
+    let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24) 
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / 1000 / 60 / 60)
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / 1000 / 60)
+    let seconds = Math.floor((dateDiff % (1000 * 60 )) / 1000 )
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days}`: days 
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}`: hours 
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}`: minutes 
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}`: seconds 
+
+
+    if(dateDiff <= 0){
+        clearInterval(counter)
+    }
+}
+, 1000)
